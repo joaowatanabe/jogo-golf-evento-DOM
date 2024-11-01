223123576a13f7ad2587b76c1bbf6c9214bc3def
wabemusic <jvwatanabe@gmail.com>
2024-11-01 00:11:28 -0300

    botao de reiniciar

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
 const bolinhaElemento = document.querySelector(".bolinha");
 
-let posicao = 0
+let posicao = 0;
 
 const moverBolinha = (event) => {
-  if(event.keyCode == 39) {
-    posicao += 10
-    bolinhaElemento.style.marginLeft = `${posicao}px`
+  if (event.keyCode == 39) {
+    posicao += 10;
+    bolinhaElemento.style.marginLeft = `${posicao}px`;
 
-    if(posicao == 400) {
+    if (posicao == 400) {
       setTimeout(() => {
-        alert("Você venceu!")
-        reiniciar()
-      }, 500)
+        alert("Você venceu!");
+        reiniciar();
+      }, 50);
     }
   }
-}
+};
 
-document.addEventListener("keydown", moverBolinha)
+document.addEventListener("keydown", moverBolinha);
+
+const reiniciar = () => {
+  posicao = 0;
+  bolinhaElemento.style.marginLeft = `${posicao}px`;
+};
