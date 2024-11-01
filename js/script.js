900f86410545fec052d2f03aeb3dda0e6fcb845d
wabemusic <jvwatanabe@gmail.com>
2024-11-01 00:05:02 -0300

    mover a bolinha para direção do buraco

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ const bolinhaElemento = document.querySelector(".bolinha");
 
 let posicao = 0
 
+const moverBolinha = (event) => {
+  if(event.keyCode == 39) {
+    posicao += 10
+    bolinhaElemento.style.marginLeft = `${posicao}px`
+  }
+}
+
+document.addEventListener("keydown", moverBolinha)
