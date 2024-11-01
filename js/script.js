7d849abb00829c6a2d5fc14776199b94003991e1
wabemusic <jvwatanabe@gmail.com>
2024-11-01 00:08:17 -0300

    alert e settimeout

--- js/script.js
+++ js/script.js
@@ -6,6 +6,13 @@ const moverBolinha = (event) => {
   if(event.keyCode == 39) {
     posicao += 10
     bolinhaElemento.style.marginLeft = `${posicao}px`
+
+    if(posicao == 400) {
+      setTimeout(() => {
+        alert("Você venceu!")
+        reiniciar()
+      }, 500)
+    }
   }
 }
 
